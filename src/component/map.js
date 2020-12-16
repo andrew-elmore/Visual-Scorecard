17862afc975c4647f033a5cc4bff6d7664ffb268
Andrew Elmore <andrewelmore93@gmail.com>
2020-12-16 12:07:35 -0500

    fix map

--- src/component/map.js
+++ src/component/map.js
@@ -6,38 +6,6 @@ import styleSettings from './../styleSettings'
 const Map = (props) => {
     let pos = props.pos
 
-
-
-
-    
-
-    // const extractLatLng = (lat, lng) => {
-    //     let latDiff = Math.max(...lat) - Math.min(...lat)
-    //     let latitude = (Math.min(...lat) + latDiff)
-    //     let latitudeDelta = (latDiff + 0.0001)
-    //     let lngDiff = Math.max(...lng) - Math.min(...lng)
-    //     let longitude = (Math.min(...lng) + lngDiff)
-    //     let longitudeDelta = (lngDiff + 0.0001)
-    //     return {
-    //         latitude: latitude,
-    //         longitude: longitude,
-    //         latitudeDelta: latitudeDelta,
-    //         longitudeDelta: longitudeDelta
-    //     }
-    // }
-
-    // if (props.shots[1].length > 0){
-    //     let lat = []
-    //     let lng = []
-    //     Object.values(props.shots).forEach((hole) => {
-    //         hole.forEach((shot) => {
-    //             lat.push(shot.coords.latitude)
-    //             lng.push(shot.coords.longitude)
-    //         })
-    //     })
-    //     pos = extractLatLng(lat, lng)
-    // }
-
     
     let shots = []
     Object.values(props.shots).forEach((hole) => {
