cb16c0114af933271edbd3bacb3c78577ac3821c
Andrew Elmore <andrewelmore93@gmail.com>
2020-12-16 22:07:24 -0500

    reorganize app to accomidate server

---
+++ server/README.md
@@ -0,0 +1,4 @@
+62zvpKmmwCOad4KQ
+dbUser
+
+mongodb+srv://dbUser:<password>@cluster0.n9rlf.mongodb.net/<dbname>?retryWrites=true&w=majority
---
+++ server/src/index.js
@@ -0,0 +1,36 @@
+require('./models/User');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri = 'mongodb+srv://dbUser:62zvpKmmwCOad4KQ@cluster0.n9rlf.mongodb.net/users?retryWrites=true&w=majority';
+if (!mongoUri) {
+  throw new Error(
+    `MongoURI was not supplied.  Make sure you watch the video on setting up Mongo DB!`
+  );
+}
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', err => {
+  console.error('Error connecting to mongo', err);
+});
+
+app.get('/', requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
